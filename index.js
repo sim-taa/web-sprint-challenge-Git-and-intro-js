8c7cbd1f77049fd4d090517463695f2a5d58ce5b
Simone Taarnskov <simone.taarnskov@gmail.com>
2022-01-16 13:39:34 -0800

    solves task 6

--- index.js
+++ index.js
@@ -287,8 +287,18 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(array) {
+  array.push(
+    { 
+      "id": 20,
+      "name": "Simone Taarnskov", 
+      "years": "1990 - current day",
+      "genre": "Web Design", 
+      "nationality": "American",
+      "bio": "Nam ratione fuga possimus. Suscipit perspiciatis labore beatae velit. Consectetur recusandae doloremque corporis.",
+    } 
+  )
+  return array
 }
 
 
